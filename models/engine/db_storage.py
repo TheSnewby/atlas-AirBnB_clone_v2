e81f6be69d6e07a1068affa5027d519f6da3d3d7
Malik Vance <malikovance@gmail.com>
2024-10-20 16:17:31 -0500

    checker wants database storage. i added the file with some code to start with.

---
+++ models/engine/db_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines the DBStorage engine."""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+# Add other model imports as needed
+
+
+class DBStorage:
+    """Represents the database storage engine."""
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize a new DBStorage instance."""
+        self.__engine = create_engine('mysql+mysqldb://user:pwd@localhost/db_name')
+
+    def all(self, cls=None):
+        """Query all objects based on class or all classes."""
+        # Implementation for querying the database
+
+    def new(self, obj):
+        """Add a new object to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to the database."""
+        self.__session.commit()
+
+    def reload(self):
+        """Reload data from the database."""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine)
+        self.__session = scoped_session(session_factory)
