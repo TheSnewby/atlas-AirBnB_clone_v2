781b9f5d067963fdc082700062ef772d2394f76a
Malik Vance <malikovance@gmail.com>
2024-10-20 16:00:42 -0500

    white space and indention

--- console.py
+++ console.py
@@ -78,8 +78,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # Check for *args or **kwargs
-                    if (pline[0] == '{' and pline[-1] == '}' and 
-                        isinstance(eval(pline), dict)):
+                    if (pline[0] == '{' and pline[-1] == '}' and
+                            isinstance(eval(pline), dict)):
 
                         _args = pline
                     else:
