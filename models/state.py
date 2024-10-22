80f587274cc1fd814f24fa0ee5b5d10d3406bdad
TheSnewby <stephen.j.newby@gmail.com>
2024-10-21 20:36:02 -0500

    task 6 State

--- models/state.py
+++ models/state.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from datetime import datetime
+from sqlalchemy import Column, ForeignKey, String
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    
+
+
+    # name = ""
