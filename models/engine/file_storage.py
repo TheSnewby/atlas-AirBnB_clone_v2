754d073015986eee52f4089ac857cf7d658dd910
Malik Vance <malikovance@gmail.com>
2024-10-22 11:32:41 -0500

    delete method should construct the key to check against stored objecet

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+import os
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -19,35 +19,31 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
+
         if cls in classes:
             cls_dict = {key: value for key, value in
-                        FileStorage.__objects.items()
-                        if isinstance(value, cls)}
+                         FileStorage.__objects.items()
+                         if isinstance(value, cls)}
             return cls_dict
         else:
             return FileStorage.__objects
-        # return FileStorage.__objects
-        # all() is not returning all new State/City created
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Adds a new object to storage dictionary."""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Saves storage dictionary to file."""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
+            temp = {key: val.to_dict() for key, val in FileStorage.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Loads storage dictionary from file."""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -57,24 +53,35 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
+
+        # Ensure the file exists
+        if not os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'w') as f:
+                json.dump({}, f)  # Create an empty file if it doesn't exist
+
         try:
-            temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
                     self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
+        except (FileNotFoundError, json.JSONDecodeError):
+            # Handle the case where the file might be empty or unreadable
             pass
 
     def delete(self, obj=None):
-        """Deletes object in database"""
-        if obj and obj in FileStorage.__objects:
-            del self.all()[obj.id]
-            self.save()
+        """Deletes an object from storage."""
+        if obj:
+            key = f"{type(obj).__name__}.{obj.id}"
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
+                self.save()
+            else:
+                print("** no instance found **")
+
             # key = f"{c_name}.{c_id}"
 
             # try:
