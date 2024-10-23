ec4719a96b8cdd03e5118a11bda16907aea598ad
Malik Vance <malikovance@gmail.com>
2024-10-23 09:50:11 -0500

    added table name and some attributes.

--- models/user.py
+++ models/user.py
@@ -4,15 +4,16 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    
+
+    # Class attributes representing columns
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
     
-    # Relationship with Place
+    # Relationship with Place (if applicable)
     places = relationship("Place", back_populates="user", cascade="all, delete-orphan")
+
