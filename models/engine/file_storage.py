efe89ff3ea4c6d6586f0b291f500993a4cb3e61b
Malik Vance <malikovance@gmail.com>
2024-10-27 12:38:49 -0500

    had merge conflict so just pushed what i had and then redid file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,10 +65,11 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Deletes an object from storage."""
-        if obj:
-            key = f"{type(obj).__name__}.{obj.id}"
-            if key in FileStorage.__objects:
-                del FileStorage.__objects[key]
-                self.save()
-            else:
-                print("** no instance found **")
+        if obj is None:
+            return
+        key = f"{type(obj).__name__}.{obj.id}"
+        if key in self.__objects:
+            del self.__objects[key]
+            self.save()
+        else:
+            raise KeyError("** no instance found **")
