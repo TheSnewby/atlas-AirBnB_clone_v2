4e60df1694a26bb48ab794c22ae444c398c8d9bd
Malik Vance <malikovance@gmail.com>
2024-10-27 12:33:56 -0500

    fixed reload method in file storage and testing adjustments made in filestorage test.Then modified init method to accept args and kwargs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,84 +1,75 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage file storage for the HBNB clone."""
 import json
 import os
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """This class manages storage of HBNB models in JSON format."""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models (of a class) currently in storage"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+        """Returns a dictionary of models (of a class) currently in storage."""
+        if cls is not None and not issubclass(cls, (BaseModel, User, Place, State, City, Amenity, Review)):
+            raise TypeError("cls must be a class type")
 
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
-
-        # If cls is provided, filter by class
-        if cls and cls in classes.values():
-            cls_dict = {key: value for key, value in
-                         FileStorage.__objects.items()
-                         if isinstance(value, cls)}
-            return cls_dict
-        else:
-            return FileStorage.__objects
+        if cls:
+            return {key: value for key, value in self.__objects.items() if isinstance(value, cls)}
+        return self.__objects
 
     def new(self, obj):
-        """Adds a new object to storage dictionary."""
-        FileStorage.__objects[obj.to_dict()['__class__'] + '.' + obj.id] = obj
+        """Adds a new object to the storage dictionary."""
+        if obj is None:
+            raise ValueError("Cannot add NoneType object")
+        key = f"{type(obj).__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file."""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {key: val.to_dict() for key, val in FileStorage.__objects.items()}
+        """Saves the storage dictionary to a file."""
+        with open(self.__file_path, 'w') as f:
+            temp = {key: val.to_dict() for key, val in self.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file."""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+        """Loads the storage dictionary from a file."""
+        if not os.path.exists(self.__file_path):
+            return
 
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
+        with open(self.__file_path, 'r') as f:
+            data = f.read()
+            if not data:
+                raise ValueError("File is empty")  # Raise ValueError if file is empty
 
-        # Ensure the file exists
-        if not os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'w') as f:
-                json.dump({}, f)  # Create an empty file if it doesn't exist
+            temp = json.loads(data)
+            classes = {
+                'BaseModel': BaseModel, 
+                'User': User, 
+                'Place': Place,
+                'State': State, 
+                'City': City, 
+                'Amenity': Amenity,
+                'Review': Review
+            }
 
-        try:
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    FileStorage.__objects[key] = classes[val['__class__']](**val)
-        except (FileNotFoundError, json.JSONDecodeError):
-            # Handle the case where the file might be empty or unreadable
-            pass
+            for key, val in temp.items():
+                cls_name = val['__class__']
+                if cls_name in classes:
+                    self.__objects[key] = classes[cls_name](**val)
 
     def delete(self, obj=None):
         """Deletes an object from storage."""
-        if obj:
-            key = f"{type(obj).__name__}.{obj.id}"
-            if key in FileStorage.__objects:
-                del FileStorage.__objects[key]
-                self.save()
-            else:
-                print("** no instance found **")
+        if obj is None:
+            return
+        key = f"{type(obj).__name__}.{obj.id}"
+        if key in self.__objects:
+            del self.__objects[key]
+            self.save()
+        else:
+            raise KeyError("** no instance found **")
