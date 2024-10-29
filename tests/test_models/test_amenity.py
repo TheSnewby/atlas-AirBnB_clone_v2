f17d9dbcb5525df6ef1399c482b430c7c6106ac5
TheSnewby <stephen.j.newby@gmail.com>
2024-10-29 18:36:02 -0500

    DBStorage tests for all models

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,7 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
-from models import storage_type
+from models import storage_type, storage
 import unittest
 
 
@@ -20,3 +20,40 @@ class test_Amenity(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+@unittest.skipUnless(storage_type == 'db', "Tests designed only for DBStorage")
+class test_amenity_DB(unittest.TestCase):
+    """Amenity DB tests"""
+    def setUp(self):
+        """Setup a new User instance before each test"""
+        self.amenity = Amenity(name="Chair")
+        storage.new(self.amenity)
+        storage.save()
+
+    def tearDown(self):
+        """Teardown of all User instances after each test"""
+        storage.delete(self.amenity)
+        storage.save()
+
+    def test_amenity_instance_in_storage(self):
+        """Test that the Amenity instance is stored in the database"""
+        key = "Amenity." + self.amenity.id
+        self.assertIn(key, storage.all())
+
+    def test_amenity_name_in_storage(self):
+        """Test that the Amenity instance in storage has the correct name"""
+        key = "Amenity." + self.amenity.id
+        self.assertEqual(storage.all()[key].name, "Chair")
+
+    def test_amenity_retrieval_by_id(self):
+        """Test retrieval of the Amenity from storage by its ID"""
+        amenity_from_storage = storage.all().get("Amenity." + self.amenity.id)
+        self.assertIsNotNone(amenity_from_storage)
+
+    def test_amenity_name_type(self):
+        """Test that the name attribute is a string"""
+        self.assertEqual(type(self.amenity.name), str)
+
+    def test_amenity_name_value(self):
+        """Test that the name matches the expected value"""
+        self.assertEqual(self.amenity.name, "Chair")
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
+from models.state import State
+from models.city import City
+from models.user import User
 from models.place import Place
-from models import storage_type
+from models import storage_type, storage
 import unittest
 
 
@@ -70,3 +73,75 @@ class test_Place(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.amenity_ids), list)
+
+@unittest.skipUnless(storage_type == 'db', "Tests designed only for DBStorage")
+class test_place_DB(unittest.TestCase):
+    """Place DB tests"""
+    def setUp(self):
+        """Setup a new Place instance before each test"""
+        self.state = State(name="Test_State")
+        storage.new(self.state)
+        storage.save()
+        self.city = City(name="Test_City", state_id=self.state.id)
+        storage.new(self.city)
+        storage.save()
+        self.user = User(email="a@b.com", password="password")
+        storage.new(self.user)
+        storage.save()
+        self.place = Place(user_id=self.user.id,
+                           city_id=self.city.id,
+                           name="Test_Place",
+                           description="Test_Description",
+                           number_rooms=3,
+                           number_bathrooms=2,
+                           max_guest=4,
+                           price_by_night=100,
+                           latitude=3.14,
+                           longitude=-3.14)
+        storage.new(self.place)
+        storage.save()
+
+    def test_city_id(self):
+        """Test city_id is a string and valid foreign key."""
+        self.assertEqual(type(self.place.city_id), str)
+        self.assertEqual(self.place.city_id, self.city.id)
+
+    def test_name(self):
+        """Test name attribute."""
+        self.assertEqual(type(self.place.name), str)
+        self.assertEqual(self.place.name, "Test_Place")
+
+    def test_description(self):
+        """Test description attribute."""
+        self.assertEqual(type(self.place.description), str)
+        self.assertEqual(self.place.description, "Test_Description")
+
+    def test_number_rooms(self):
+        """Test number_rooms attribute."""
+        self.assertEqual(type(self.place.number_rooms), int)
+        self.assertEqual(self.place.number_rooms, 3)
+
+    def test_number_bathrooms(self):
+        """Test number_bathrooms attribute."""
+        self.assertEqual(type(self.place.number_bathrooms), int)
+        self.assertEqual(self.place.number_bathrooms, 2)
+
+    def test_max_guest(self):
+        """Test max_guest attribute."""
+        self.assertEqual(type(self.place.max_guest), int)
+        self.assertEqual(self.place.max_guest, 4)
+
+    def test_price_by_night(self):
+        """Test price_by_night attribute."""
+        self.assertEqual(type(self.place.price_by_night), int)
+        self.assertEqual(self.place.price_by_night, 100)
+
+    def test_latitude(self):
+        """Test latitude attribute."""
+        self.assertEqual(type(self.place.latitude), float)
+        self.assertEqual(self.place.latitude, 3.14)
+
+    def test_longitude(self):
+        """Test longitude attribute."""
+        self.assertEqual(type(self.place.longitude), float)
+        self.assertEqual(self.place.longitude, -3.14)
