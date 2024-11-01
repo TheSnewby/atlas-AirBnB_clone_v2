74ad5ab270851db637d5fb3372eb131261fa0ba8
TheSnewby <stephen.j.newby@gmail.com>
2024-11-01 18:03:31 -0500

    4-number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Multiple pages"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def texts(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python/", defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythons(text):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def numbers(n):
+    if n.isdigit():
+        return n + " is a number"
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
