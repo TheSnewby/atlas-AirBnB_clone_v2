1326a3070431509f440e68a6b4406be6598112cc
TheSnewby <stephen.j.newby@gmail.com>
2024-11-01 17:40:54 -0500

    3-python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Multiple pages"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def texts(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pythons(text):
+    if text:
+        return "Python " + text.replace("_", " ")
+    else:
+        return "Python is cool"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
