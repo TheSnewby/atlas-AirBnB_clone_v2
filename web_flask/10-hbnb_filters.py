261ae6923a060eef192ff84c95f97acc278be0e9
TheSnewby <stephen.j.newby@gmail.com>
2024-11-05 23:51:07 -0600

    updated 10-html

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""9. Cities by States"""
+"""10. States and State"""
 from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
